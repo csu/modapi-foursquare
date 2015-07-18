1446c39ea5121e4b070b89ff735dc419073e67b1
Christopher Su <christophersu9@gmail.com>
2015-07-17 22:51:52 -0700

    better secret handling

--- routes.py
+++ routes.py
@@ -1,9 +1,10 @@
 import thread
 import time
 
-from flask import Blueprint, request, abort, jsonify
+from flask import Blueprint, request, jsonify
 import foursquare
 
+from common import require_secret
 from config import config
 import secrets
 
@@ -11,40 +12,29 @@ module = Blueprint(config['module_name'], __name__)
 
 client = foursquare.Foursquare(access_token=secrets.FOURSQUARE_ACCESS_TOKEN)
 
-def secret_correct():
-    if secret_key and secret_key_value:
-        val = request.args.get(secret_key)
-        if val == secret_key_value:
-            return True
-    return False
-
+@require_secret
 def perform_checkins(places):
     for place in places:
         client.checkins.add(params={'venueId': place})
         time.sleep(0.5)
 
 @module.route('/')
+@require_secret
 def get_all_checkins():
-    if not secret_correct():
-        abort(403)
     return jsonify(client.users.checkins())
 
 @module.route('/checkin')
 @module.route('/checkin/')
+@require_secret
 def do_checkins():
-    if not secret_correct():
-        abort(403)
-
     places = request.args.get('p').split(',')
     perform_checkins(places)
     return jsonify({'status': 'ok', 'places': places})
 
 @module.route('/recent')
 @module.route('/recent/')
+@require_secret
 def get_recent():
-    if not secret_correct():
-        abort(403)
-
     latest = client.users.checkins(params={'limit': 3})['checkins']['items']
     latest = [i['venue']['name'] for i in latest]
 
